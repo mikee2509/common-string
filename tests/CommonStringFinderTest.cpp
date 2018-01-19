13d7f6c32d6c1c1b8371211f2a434cb8f7dcf27e
Michał Sieczkowski <mikee2509@gmail.com>
2018-01-19 03:30:00 +0100

    Tests improvements

--- tests/CommonStringFinderTest.cpp
+++ tests/CommonStringFinderTest.cpp
@@ -26,13 +26,21 @@ protected:
 };
 
 
+static ulong numSuccesses = 0;
+
 // Check if solutions found by heuristic are correct
-class HeuristicCorrectnessTest : public CommonStringFinderTest {};
+class HeuristicCorrectnessTest : public CommonStringFinderTest {
+public:
+    static void TearDownTestCase() {
+        cout << "Number of solutions checked for correctness: " << numSuccesses << endl;
+    }
+};
 
 TEST_P(HeuristicCorrectnessTest, T1) {
     auto result = csf.heuristic(*set);
     if (result.solutionFound()) {
         EXPECT_TRUE(csf.doesKeyMatch(result.value, *set));
+        ++numSuccesses;
         return;
     }
 }
@@ -41,19 +49,19 @@ INSTANTIATE_TEST_CASE_P(CSFTests, HeuristicCorrectnessTest,
                         Combine(Range(ulong(10), ulong(100), ulong(1)),
                                 Range(ulong(10), ulong(1000), ulong(10))));
 
-
 // Check heuristic success rate
 class HeuristicSuccessRateTest : public CommonStringFinderTest {
 protected:
     const ulong numAttempts = 1000;
+    const int minSuccessRate = 90;
     const int w = 4;
 };
 
-static map<ulong, double> rates;
+static vector<double> rates(15, -1.0);
 
 TEST_P(HeuristicSuccessRateTest, T2) {
-    auto it = rates.find(set->getStringLength());
-    if (it != rates.end() && (isnan(it->second) || it->second < 90)) {
+    ulong strLen = set->getStringLength();
+    if (rates[strLen] != -1.0 && (isnan(rates[strLen]) || rates[strLen] < minSuccessRate)) {
         return;
     }
     ulong bruSuccesses = 0;
@@ -70,17 +78,17 @@ TEST_P(HeuristicSuccessRateTest, T2) {
         rsg.fillStringSet(*set);
     }
     double sr = (double) heuSuccesses * 100 / bruSuccesses;
-    rates[set->getStringLength()] = sr;
-    std::cout << std::endl
-              << "strLen: " << set->getStringLength()
-              << "\tnumStr: " << set->getNumStrings()
-              << "\t" << setw(w) << right << heuSuccesses << "/" << setw(w) << left << bruSuccesses
-              << "\tSR = " << fixed << right << setw(5) << setprecision(1) << sr << " %"
-              << std::endl;
+    rates[strLen] = sr;
+    cout << endl
+         << "strLen: " << strLen
+         << "\tnumStr: " << set->getNumStrings()
+         << "\t" << setw(w) << right << heuSuccesses << "/" << setw(w) << left << bruSuccesses
+         << "\tSR = " << fixed << right << setw(5) << setprecision(1) << sr << " %"
+         << endl;
 }
 
 INSTANTIATE_TEST_CASE_P(CSFTests, HeuristicSuccessRateTest,
                         Combine(Range(ulong(1), ulong(15), ulong(1)),
-                                Range(ulong(1), ulong(100), ulong(1))));
+                                Range(ulong(1), ulong(1000), ulong(5))));
 
 
